f0b3b64218a6cd9e6a2401b67fa32e830521c79d
Yetunde Ajayi <olayemioluwa17@gmail.com>
2023-05-01 17:12:47 -0600

    created the react application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+export default function App() {
+  let [city, setCity] = useState(" ");
+  let [temp, setTemp] = useState(" ");
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
 
-function App() {
+  function showWeather(response) {
+    console.log(response.data);
+    setTemp(
+      <ul>
+        <li>{Math.round(response.data.main.temp)}°C</li>
+      </ul>
+    );
+  }
+
+  function handleWeather(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8c48afa47a9a9c24f3500c7039d50aaa&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Weather">
+      <form onSubmit={handleWeather}>
+        <input type="search" placeholder="Type city" onChange={updateCity} />
+        <button type="button" className="btn btn-primary">
+          Search
+        </button>
+      </form>
+      <div>{temp}</div>
     </div>
   );
 }
-
-export default App;

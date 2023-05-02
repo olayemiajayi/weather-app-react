a0952bd4092290d8b6fe34dabbb44fae441b6fe4
Yetunde Ajayi <olayemioluwa17@gmail.com>
2023-05-01 22:18:33 -0600

    Updated the weather.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,23 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+import "./App.css";
+
 export default function App() {
-  let [city, setCity] = useState(" ");
-  let [temp, setTemp] = useState(" ");
-  function updateCity(event) {
-    event.preventDefault();
-    setCity(event.target.value);
-  }
-
-  function showWeather(response) {
-    console.log(response.data);
-    let Temperature = `${city} ${Math.round(response.data.main.temp)}°C`;
-    setTemp(
-      <ul>
-        <div className="city">
-          <li>
-            <strong>{Temperature}</strong>
-          </li>
-        </div>
-        <li>
-          {" "}
-          <img
-            src={`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
-            alt={response.data.weather[0].description}
-          />
-        </li>
-        <div className="wind">
-          <strong>
-            <li>Wind: {response.data.wind.speed} Km/h</li>
-            <li>Humidity: {response.data.main.humidity}</li>
-          </strong>
-        </div>
-      </ul>
-    );
-  }
-
-  function handleWeather(event) {
-    event.preventDefault();
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8c48afa47a9a9c24f3500c7039d50aaa&units=metric`;
-    axios.get(apiUrl).then(showWeather);
-  }
   return (
-    <div className="Weather">
-      <form onSubmit={handleWeather}>
-        <input type="search" placeholder="Type city" onChange={updateCity} />
-        <input type="submit" value="search" />
-      </form>
-      <h1>London</h1>
-      <h2>{temp}</h2>
+    <div className="App">
+      <div className="container">
+        <small>
+          <span className="name">
+            <a
+              rel="noreferrer"
+              href="https://github.com/olayemiajayi/weather-app-react"
+              target="_blank"
+            >
+              Open-source code
+            </a>
+            by Yetunde Ajayi
+          </span>
+        </small>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function App() {
+  let [city, setCity] = useState(" ");
+  let [temp, setTemp] = useState(" ");
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  function showWeather(response) {
+    console.log(response.data);
+    let Temperature = `${city} ${Math.round(response.data.main.temp)}°C`;
+    setTemp(
+      <ul>
+        <div className="city">
+          <li>
+            <strong>{Temperature}</strong>
+          </li>
+        </div>
+        <li>
+          {" "}
+          <img
+            src={`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+            alt={response.data.weather[0].description}
+          />
+        </li>
+        <div className="wind">
+          <strong>
+            <li>Wind: {response.data.wind.speed} Km/h</li>
+            <li>Humidity: {response.data.main.humidity}</li>
+          </strong>
+        </div>
+      </ul>
+    );
+  }
+
+  function handleWeather(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8c48afa47a9a9c24f3500c7039d50aaa&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+  return (
+    <div className="Weather">
+      <form onSubmit={handleWeather}>
+        <input type="search" placeholder="Type city" onChange={updateCity} />
+        <input
+          type="submit"
+          value="search"
+          className="btn btn-primary w-4 m-2"
+        />
+      </form>
+
+      <h2>{temp}</h2>
+    </div>
+  );
+}

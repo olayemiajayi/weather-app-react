75c47e42c3dbab2f3126d89b80cc8ad07c89b961
Yetunde Ajayi <olayemioluwa17@gmail.com>
2023-05-01 18:57:28 -0600

    updated the react app

--- src/App.js
+++ src/App.js
@@ -12,14 +12,26 @@ export default function App() {
     console.log(response.data);
     setTemp(
       <ul>
-        <li>{Math.round(response.data.main.temp)}°C</li>
+        <div className="city">
+          <li>
+            <strong>
+              {city} {Math.round(response.data.main.temp)}°C
+            </strong>
+          </li>
+        </div>
         <li>
           {" "}
           <img
-            src={`https://openweathermap.org/img/wn/{response.data.weather[0].icon}@2x.png`}
+            src={`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
             alt={response.data.weather[0].description}
           />
         </li>
+        <div className="wind">
+          <strong>
+            <li>Wind: {response.data.wind.speed} Km/h</li>
+            <li>Humidity: {response.data.main.humidity}</li>
+          </strong>
+        </div>
       </ul>
     );
   }
@@ -33,8 +45,11 @@ export default function App() {
     <div className="Weather">
       <form onSubmit={handleWeather}>
         <input type="search" placeholder="Type city" onChange={updateCity} />
-        <input type="submit" value="search" />
+        <input type="submit" value="search" class="btn btn-primary w-48 m-2" />
       </form>
+      <h2>London</h2>
+      <h2>Friday 05:00</h2>
+
       <div>{temp}</div>
     </div>
   );
